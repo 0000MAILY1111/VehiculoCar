f6f28205916f53c2903ee3cd72c8a7e2868ee868
0000MAILY1111 <valdezmelizza19@gmail.com>
2026-02-28 16:31:53 -0400

    Implement new form and refactor business logic integration with presentation layer

--- src/mvc/cliente/CCliente.java
+++ src/mvc/cliente/CCliente.java
@@ -5,10 +5,34 @@
  */
 package mvc.cliente;
 
+import java.util.Map;
+
 /**
  *
  * @author valde
  */
 public class CCliente {
-    
+     private MCliente DatoCliente;
+
+    public CCliente(MCliente DatoCliente) {
+        this.DatoCliente = DatoCliente;
+    }
+
+  
+
+    public void cargarDatos(Map<String, Object> data) {
+        DatoCliente.cargarDatos(data);
+    }
+
+    public Object[] guardar() {
+        return DatoCliente.guardar();
+    }
+
+    public Map<String, Object[]> listar() {
+        return DatoCliente.listar();
+    }
+
+    public void eliminar(String id) {
+        DatoCliente.eliminar(id);
+    }
 }
